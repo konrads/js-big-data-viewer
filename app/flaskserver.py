0c6b2dce824312c1b6100dc5909cab126b4d4452
Konrad <sosnowski.konrad@gmail.com>
2013-08-15 12:14:39 +0100

    fixing for Firefox - date string in js, borders css

--- app/flaskserver.py
+++ app/flaskserver.py
@@ -14,36 +14,12 @@ DATE_PATTERN = '%Y-%m-%d'
 
 @app.route('/portfolio-angular')
 def portfolio_angular():
-    cols = {'cnt': len(db.dates), 'visible': 10, 'buffer': 50}
-    rows = {'cnt': len(db.symbols), 'visible': 5, 'buffer': 5}
-    initial_data = db.data(
-        db.symbols,
-        db.start_date,
-        db.start_date + timedelta(days=cols['visible']+cols['buffer']))
-    return render_template(
-        'portfolio-angular.html',
-        symbols=db.symbols,
-        start_date=db.start_date,
-        cols=cols,
-        rows=rows,
-        initial_data=initial_data)
+    return _portfolio('portfolio-angular.html')
 
 
 @app.route('/portfolio')
 def portfolio():
-    cols = {'cnt': len(db.dates), 'visible': 10, 'buffer': 50}
-    rows = {'cnt': len(db.symbols), 'visible': 5, 'buffer': 5}
-    initial_data = db.data(
-        db.symbols,
-        db.start_date,
-        db.start_date + timedelta(days=cols['visible']+cols['buffer']))
-    return render_template(
-        'portfolio.html',
-        symbols=db.symbols,
-        start_date=db.start_date,
-        cols=cols,
-        rows=rows,
-        initial_data=initial_data)
+    return _portfolio('portfolio.html')
 
 
 @app.route('/api/symbols/<symbols>/start_date/<start_date>/end_date/<end_date>')
@@ -53,6 +29,22 @@ def partial(symbols, start_date, end_date):
     end_date = datetime.strptime(end_date, DATE_PATTERN)
     print symbols, start_date, end_date
     return json.dumps(db.data(symbols, start_date, end_date))
+
+
+def _portfolio(template):
+    cols = {'cnt': len(db.dates), 'visible': 10, 'buffer': 50}
+    rows = {'cnt': len(db.symbols), 'visible': 5, 'buffer': 5}
+    initial_data = db.data(
+        db.symbols,
+        db.start_date,
+        db.start_date + timedelta(days=cols['visible']+cols['buffer']))
+    return render_template(
+        template,
+        symbols=db.symbols,
+        start_date=db.start_date.strftime(DATE_PATTERN),
+        cols=cols,
+        rows=rows,
+        initial_data=initial_data)
 
 
 if __name__ == "__main__":
